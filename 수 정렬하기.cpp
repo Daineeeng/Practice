1c64e44b61e0a96806ce92734d9e9a8c64299728
Dain <30483467+Daineeeng@users.noreply.github.com>
2019-04-30 15:15:28 +0900

    https://www.acmicpc.net/problem/2750

--- 수 정렬하기.cpp
+++ 수 정렬하기.cpp
@@ -1,4 +1,4 @@
-//Selection Sort
+// Selection Sort
 
 #include <iostream>
 using namespace std;
@@ -6,18 +6,54 @@ using namespace std;
 int N; int nums[MAXN];
 int main() {
 	ios::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+	freopen("Input.txt", "r", stdin);
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> nums[i];
 	}
 	for (int i = 0; i < N; i++) {
+		int min = i;
 		for (int j = i + 1; j < N; j++) {
-			if (nums[j] < nums[i]) {
-				swap(nums[i], nums[j]);
+			if (nums[j] < nums[min]) min = j;
+		}
+		swap(nums[i], nums[min]);
+		//cout << i + 1 << "단계 ";
+		//for (int i = 0; i < N; i++) cout << nums[i] << " ";
+		//cout << '\n';
+	}
+	for (int i = 0; i < N; i++) {
+		cout << nums[i] << '\n';
+	}
+}
+
+
+// Bubble Sort
+
+#include <iostream>
+using namespace std;
+#define MAXN 1000
+int N; int nums[MAXN];
+int main() {
+	ios::sync_with_stdio(0); cout.tie(0); cin.tie(0);
+	freopen("Input.txt", "r", stdin);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> nums[i];
+	}
+	for (int i = N; i > 0; i--) {
+		//cout << N - i + 1 << "단계\n";
+		for (int j = 0; j < i; j++) {
+			if (nums[j] > nums[j + 1]) {
+				int temp = nums[j];
+				nums[j] = nums[j + 1];
+				nums[j + 1] = temp;
 			}
+			//for (int i = 0; i < N; i++) cout << nums[i] << " ";
+			//cout << '\n';
 		}
 	}
 	for (int i = 0; i < N; i++) {
 		cout << nums[i] << '\n';
 	}
 }
+
