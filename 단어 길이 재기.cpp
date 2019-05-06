8676f498474713add9be4fe90c5b2f55198f0db7
Dain <30483467+Daineeeng@users.noreply.github.com>
2019-05-06 23:41:44 +0900

    https://www.acmicpc.net/problem/2743

---
+++ 단어 길이 재기.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	string s; cin >> s; cout << s.length() << '\n';
+	return 0;
+}
