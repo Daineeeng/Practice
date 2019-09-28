b800d3e42ed478cdf79aebbab49e80e09f3f58c2
Dain <30483467+Daineeeng@users.noreply.github.com>
2019-09-28 17:27:51 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12919

--- 서울에서 김서방 찾기.cpp
+++ 서울에서 김서방 찾기.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 using namespace std;
 
 string solution(vector<string> seoul) {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int idx=0;
-    for(int i=0; i<seoul.size(); i++){
-        if(seoul[i]=="Kim") idx=i;
-    }
     string answer = "김서방은 ";
-    answer += to_string(idx);
+    for(int i=0; i<(int)seoul.size(); i++) {
+        if (seoul[i] == "Kim") {
+            answer += to_string(i);
+            break;
+        }
+    }
     answer += "에 있다";
     return answer;
 }
