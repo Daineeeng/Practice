e19c6a6ecd2650dc012fa78d20b3ed69fd5f5be1
Dain <30483467+Daineeeng@users.noreply.github.com>
2019-09-28 17:04:09 +0900

    https://programmers.co.kr/learn/courses/30/lessons/12917

---
+++ 문자열 내림차순으로 배열하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string s) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    sort(s.rbegin(), s.rend());
+    string answer = s;
+    return answer;
+}
