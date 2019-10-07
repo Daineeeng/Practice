fb36e223582c63382b49871bba68f615275c8706
Dain <30483467+Daineeeng@users.noreply.github.com>
2019-10-07 20:08:28 +0900

    https://swexpertacademy.com/main/solvingProblem/solvingProblem.do

---
+++ String.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	freopen("input.txt", "r", stdin);
+
+	int tc, TC, ans;
+	string f, s;
+	for (tc = 1; tc <= 10; ++tc)
+	{
+		cin >> TC;
+		cin >> f;
+		cin >> s;
+		ans = 0;
+
+		int flen = f.length();
+		int slen = s.length();
+
+		for (int i = 0; i <= slen - flen; i++) {
+			string tmp = "";
+			for (int j = i; j < i + flen; j++) {
+				tmp += s[j];
+				if (tmp == f) ans++;
+			}
+		}
+
+		cout << '#' << TC << ' ' << ans << '\n';
+	}
+	return 0;
+}
